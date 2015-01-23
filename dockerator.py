7f80a05912f9e25c9262abd6ed5199004f5766f9
drets <dmytro.rets@wikia-inc.com>
2015-01-23 21:42:00 +0100

    CR fixes.

--- dockerator.py
+++ dockerator.py
@@ -5,16 +5,17 @@ import argparse
 import yaml
 
 from docker.client import Client
-from docker.utils import kwargs_from_env
 
 
-def process(commands):
+def process(base_url, commands):
 
     def get_image_name():
+        # image_info is dict.
+        # RepoTags key has ['image_name:tag', ...] value.
         image_info = max(cli.images(), key=lambda i: i['Created'])
         return image_info['RepoTags'][0].split(':')[0]
 
-    cli = Client(**kwargs_from_env())
+    cli = Client(base_url)
     cli.import_image(src=commands['path_to_image'],
                      repository=commands['path_to_image'])
     host_port, container_port = map(int, commands['ports'].split(':'))
@@ -29,10 +30,13 @@ def run():
     parser = argparse.ArgumentParser(description='Dockerator')
     parser.add_argument('-s', '--source',
                         help='Path to source ymal file', required=True)
-    source = vars(parser.parse_args())['source']
-    with open(source) as f:
+    parser.add_argument('-b', '--host',
+                        help='Docker host server base url',
+                        required=True)
+    args = vars(parser.parse_args())
+    with open(args['source']) as f:
         commands = yaml.load(f)
-        process(commands)
+        process(args['host'], commands)
 
 
 if __name__ == '__main__':
--- dockerator_tests.py
+++ dockerator_tests.py
@@ -7,17 +7,24 @@ import time
 class DockerSingleImage(unittest.TestCase):
 
     def test_run_single_image(self):
-        cmd = ['python', 'dockerator.py', '-s', 'single_app/build.yaml']
-        sp.Popen(cmd, stdout=sp.PIPE).communicate()
-        timeout = time.time() + 600  # 10 minutes timeout
-        while time.time() < timeout:
-            try:
-                url = 'http://192.168.59.103:1111/'
-                if urllib2.urlopen(url).read() == 'Hello world':
-                    return "Great success"
-            except:
-                time.sleep(30)
-        raise Exception
+
+        def run_dockerator(host, path_to_image):
+            cmd = ['python', 'dockerator.py', '-s', path_to_image,
+                   '-b', host]
+            sp.Popen(cmd, stdout=sp.PIPE).communicate()
+
+        def http_get(url, timeout):
+            start_time = time.time()
+            while time.time() - start_time < timeout:
+                try:
+                    return urllib2.urlopen(url).read()
+                except:
+                    continue
+
+        run_dockerator('tcp://192.168.59.103:2375',
+                       'resources/simple-web-app/build.yaml')
+        assert http_get('http://192.168.59.103:1111/', 600) == 'Hello world'
+
 
 if __name__ == '__main__':
     unittest.main()
